68ada5d20c53195b4ba38019cfec7b2d73d36156
Javier Alvarez <alvarez15@avaya.com>
2018-09-24 15:52:41 -0300

    Add project files.

---
+++ OdeToFood/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OdeToFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
